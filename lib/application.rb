6cc20bd33b96ed8f209f97ee6e2d2395dddcd69f
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2013-04-17 23:13:53 -0400

    Some basic styles

--- lib/application.rb
+++ lib/application.rb
@@ -1,11 +1,23 @@
 require 'sinatra/base'
 require 'haml'
 require 'lakrits'
+require 'sass'
+require 'compass'
 
-Lakrits::Log.level = Lakrits::Log::INFO
+Lakrits::Log.level = Lakrits::Log::WARNING
 $lakrits = Lakrits.new
 
 class Application < Sinatra::Base
+
+  configure do
+    Compass.configuration do |config|
+      config.project_path = File.dirname(__FILE__)
+      config.sass_dir = 'assets'
+    end
+
+    set :haml, { :format => :html5 }
+    set :scss, Compass.sass_engine_options
+  end
 
   get '/' do
     haml :index
@@ -17,14 +29,19 @@ class Application < Sinatra::Base
   end
 
   def open_door
-    puts "Opening door"
+    puts "Opening door.."
     msg = { 
       :recipient_id => 2,  # The hard coded ID of the door opener Arduino.
       :message_type => 1,  # Sending an open command
-      :data => [10]       # The duration of the 'button press' in multiples of 100ms (255 is the highest allowed value)
+      :data => [30]       # The duration of the 'button press' in multiples of 100ms (255 is the highest allowed value)
     }
 
     $lakrits.deliver msg
   end
+
+  get '/app.css' do
+    scss :'stylesheets/app'
+  end
+
 
 end
